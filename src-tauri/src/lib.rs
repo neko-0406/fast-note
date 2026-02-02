cdaa3291ef8dfee807b311ea873b0f361aa44cea
ryoya <sateraito3696@gmail.com>
2026-02-02 15:30:20 +0900

    envとsqliteファイルを追加

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,10 @@
 use std::{
-    fs::File,
+    fs::{self, File},
     io::{BufReader, BufWriter},
+    sync::Mutex,
 };
 
 use tauri::{
-    async_runtime::Mutex,
     menu::{MenuBuilder, SubmenuBuilder},
     App, AppHandle, Manager,
 };
@@ -22,8 +22,8 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .setup(|app| {
-            setup_window_menu(&app)?;
-            setup_config(&app)?;
+            setup_config(&app);
+            setup_window_menu(&app);
             Ok(())
         })
         .plugin(tauri_plugin_opener::init())
@@ -33,52 +33,41 @@ pub fn run() {
         .expect("error while running tauri application");
 }
 
-fn setup_config(app: &App) -> anyhow::Result<()> {
+// アプリの設定ファイルと設定の初期化
+fn setup_config(app: &App) {
     let app_handle = app.app_handle();
-    let data_dir = app_handle.path().app_data_dir()?;
-    let config_json_path = data_dir.join("app_config.json");
-
-    if !config_json_path.exists() {
-        let config_file = File::create(&config_json_path)?;
-        let writer = BufWriter::new(config_file);
-
-        let app_config = AppConfig::new();
-        serde_json::to_writer_pretty(writer, &app_config)?;
-    }
-
-    let config_file = File::open(&config_json_path)?;
-    let reader = BufReader::new(config_file);
-
-    let app_config: AppConfig = serde_json::from_reader(reader)?;
-    let mutex_app_config = Mutex::new(app_config);
-    app.manage(mutex_app_config);
-    Ok(())
 }
 
-fn setup_window_menu(app: &App) -> anyhow::Result<()> {
+// windowメニューの初期化
+fn setup_window_menu(app: &App) {
     let file_menu = SubmenuBuilder::new(app, "ファイル")
         .text("open_dir", "フォルダを開く")
-        .build()?;
+        .build()
+        .unwrap();
 
-    let window_menu = MenuBuilder::new(app).items(&[&file_menu]).build()?;
+    let window_menu = MenuBuilder::new(app).items(&[&file_menu]).build().unwrap();
 
-    app.set_menu(window_menu)?;
+    app.set_menu(window_menu).unwrap();
 
     app.on_menu_event(move |app_handle: &AppHandle, event| {
         println!("menu event {:?}", event.id());
         match event.id().0.as_str() {
             "open_dir" => {
-                let selected_dir = app_handle.dialog()
+                let selected_dir = app_handle
+                    .dialog()
                     .file()
                     .set_title("作業用のフォルダを選択してください")
                     .blocking_pick_folder();
                 match selected_dir {
-                    Some(dir_path) => {},
+                    Some(dir_path) => {
+                        let config = app_handle.state::<Mutex<AppConfig>>();
+                        let mut lock = config.lock().unwrap();
+                        lock.work_dir = dir_path.to_string();
+                    }
                     None => {}
                 }
             }
             _ => {}
         }
     });
-    Ok(())
 }
