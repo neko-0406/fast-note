7ce2575d887b393a5e4935e55d1ade0e003525a3
momiji <sateraito3696@gmail.com>
2026-02-03 10:41:38 +0900

    Implement SideMenubar layout and styles

--- src/components/SideMenubar.tsx
+++ src/components/SideMenubar.tsx
@@ -1,7 +1,19 @@
-import { ReactElement } from 'react';
+import { ReactElement, useCallback, useState } from 'react';
 
 import style from '../styles/SideMenubar.module.css';
 
 export default function SideMenubar(): ReactElement {
-  return <div className={style.Container}></div>;
+  const [showContent, setShowContent] = useState<boolean>(false);
+  const [menuWidth, setMenuWidth] = useState<number>(50);
+  
+  const dragMenuWidth = useCallback((move_x: number) => {
+    
+  }, [])
+  
+  return (
+    <div className={style.Container} style={{width: `${menuWidth}px`}}>
+      <div className={style.IconArea}></div>
+      {showContent ? <div className={style.ContentArea}></div> : null}
+    </div>
+  );
 }
