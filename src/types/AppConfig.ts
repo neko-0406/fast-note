c2bb939fb67c277d5d2a5d7f684a222264e94924
ryoya <sateraito3696@gmail.com>
2026-02-05 16:49:43 +0900

    AppState追加

--- src/types/AppConfig.ts
+++ src/types/AppConfig.ts
@@ -1,4 +1,10 @@
+import { FileItem } from './FileTree';
+
 export interface AppConfig {
   theme: 'System' | 'Dark' | 'Light';
   workDir: string;
+}
+
+export interface AppState {
+  fileTreeItem: FileItem;
 }
