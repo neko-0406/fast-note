75b095f8daa6fedeede41f472b7784d6057e9616
ryoya <sateraito3696@gmail.com>
2026-02-05 16:49:16 +0900

    AppStateの追加

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,17 @@ import { AppConfigProvider } from './contexts/AppConfig';
 import MainContent from './components/MainContent';
 import StatusBar from './components/Statusbar';
 import SideMenubar from './components/SideMenubar';
+import { AppStateProvider } from './contexts/AppState';
 
 function App(): ReactElement {
   return (
     <AppConfigProvider>
-      <MainContent>
-        <SideMenubar />
-      </MainContent>
-      <StatusBar />
+      <AppStateProvider>
+        <MainContent>
+          <SideMenubar />
+        </MainContent>
+        <StatusBar />
+      </AppStateProvider>
     </AppConfigProvider>
   );
 }
---
+++ src/contexts/AppState.tsx
@@ -0,0 +1,41 @@
+import { createContext, Dispatch, ReactNode, SetStateAction, useContext, useEffect, useMemo, useState } from 'react';
+import { FileItem } from '../types/FileTree';
+import { invoke } from '@tauri-apps/api/core';
+
+interface AppStateProps {
+  fileTree: FileItem | null;
+  setFileItem: Dispatch<SetStateAction<FileItem | null>>;
+}
+
+export const AppStateContext = createContext<AppStateProps | null>(null);
+
+export function useAppState(): AppStateProps {
+  const context = useContext(AppStateContext);
+  if (!context) {
+    throw new Error('useAppContext must be used within a AppContextProvider');
+  }
+  return context;
+}
+
+export function AppStateProvider({ children }: { children: ReactNode }) {
+  const [fileItem, setFileItem] = useState<FileItem | null>(null);
+
+  useEffect(() => {
+    const loadFileTree = async () => {
+      let fItem: FileItem = await invoke('get_work_dir_tree');
+      setFileItem(fItem);
+    };
+
+    loadFileTree();
+  }, []);
+
+  const defaultValue = useMemo(
+    () => ({
+      fileTree: fileItem,
+      setFileItem: setFileItem,
+    }),
+    [fileItem, setFileItem],
+  );
+
+  return <AppStateContext.Provider value={defaultValue}>{children}</AppStateContext.Provider>;
+}
