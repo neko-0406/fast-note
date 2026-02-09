63cf12a78a7af1c85a71561282c5d704ee415d5f
momiji <sateraito3696@gmail.com>
2026-02-09 20:37:40 +0900

    Add TabContainer structure and Tab component
    Add TabContainer props and set up TabIndexContainer and
    TabValueContainer classNames. Introduce Tab component stub with
    children handling and a selected state placeholder. Make TabValueContainer
    flex in CSS.

--- src/components/Tab.tsx
+++ src/components/Tab.tsx
@@ -1,18 +1,36 @@
-import { useState } from "react";
+import { Activity, ReactNode, useState } from "react";
 import style from "../styles/Tab.module.css";
 
-export default function TabContainer() {
+// タブをまとめるコンテナのコンポーネント
+interface TabContainerProps {}
+
+export default function TabContainer({ }: TabContainerProps) {
   const [selectedTabId, setSelectedTabId] = useState<string>("");
-  
   
   return (
     <div className={style.Container}>
       {/* タブの名前を表示するところ */}
-      <div></div>
+      <div className={style.TabIndexContainer}></div>
       {/* タブの中身を表示するところ */}
-      <div></div>
+      <div className={style.TabValueContainer}></div>
     </div>
   );
 }
 
-export function Tab() {}
+// タブ本体
+interface TabProps {
+  selectedId: string
+  children: ReactNode
+}
+
+export function Tab({ children }: TabProps) {
+  const [selected, setSelected] = useState<boolean>(false);
+  
+  return (
+    <div>
+      {/*<Activity mode={}>*/}
+        {children}
+      {/*</Activity>*/}
+    </div>
+  )
+}
